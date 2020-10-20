2e748f63917ef7795285247681200843a47e436c
Balazs <balazs@otakomaiya.com>
2020-10-21 00:39:30 +0100

    Project scaffolding, tailwindui, page structrue

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  parser: "babel-eslint",
+  "rules": {
+    "react/react-in-jsx-scope": "off"
+  },
+}
